db2d438327343cee507673f792e2037df1c0c56c
Cambell Prince <cambell_prince@sil.org>
2009-02-20 09:24:26 +0700

    Removed Thumbs.db

--- JWTools/AssemblyInfo.cs
+++ JWTools/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Runtime.CompilerServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.3")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
